87ae6be05485c8f1afbacf4c09f5fdd91e3fd481
Naishadh123 <84559186+Naishadh123@users.noreply.github.com>
2022-11-06 14:41:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ poseNet.on('pose',gotPoses);
 function draw()
 {
 image(video,0,0,600,500);
+if(scoreLeftWristY>0.2){
 fill("#FF0000");
 stroke("FF0000");
 circle(leftWristX,leftWristY,20);
@@ -30,6 +31,7 @@ remove_decimals=floor(inNumberleftWristY);
 volume=remove_decimals/500;
 document.getElementById("volume").innerHTML="Volume= "+volume;
 song.setVolume(volume);
+}
 }
 
 function play()
@@ -49,6 +51,8 @@ function gotPoses(results)
 if(results.length>0)
 {
 console.log(results);
+scoreLeftWristY=results[0].pose.keypoints[9].score;
+console.log(scoreLeftWristY)
 leftWristX=results[0].pose.leftWrist.x;
 leftWristY=results[0].pose.leftWrist.y;
 console.log("leftWristX="+leftWristX+" leftWristY="+leftWristY);
